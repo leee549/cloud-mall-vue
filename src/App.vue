9c5bc5e784f579f10e90f1f047915ce42732391e
leee549 <1193106371@qq.com>
2020-10-01 09:08:37 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,13 @@
   export default {
   }
 </script>
+<style lang="less">
+  /* 解决element-ui的table表格控件表头与内容列不对齐问题 */
+  .el-table th.gutter{
+    display: table-cell!important;
+  }
+  .el-table__row{
+  td:not(.is-hidden):last-child{
+    right:-1px;
+  }}
+</style>
